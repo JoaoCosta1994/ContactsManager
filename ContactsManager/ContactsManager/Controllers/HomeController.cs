8d70350961c61c4b52a2dd6658c3c00bc0d8b7ca
JoaoCosta1994 <joaocostaworks@gmail.com>
2022-08-06 17:54:22 +0100

    other commit

--- ContactsManager/ContactsManager/Controllers/HomeController.cs
+++ ContactsManager/ContactsManager/Controllers/HomeController.cs
@@ -10,11 +10,22 @@ namespace ContactsManager.Controllers
     
     public class HomeController : Controller
     {
+        public bool contemLetras(string texto)
+        {
+            if (texto.Where(c => char.IsLetter(c)).Count() > 0)
+                return true;
+            else
+                return false;
+        }
         public bool verifica = true;
         [HttpGet]
         public ViewResult Index()
         {
-           
+            if (listatelefonica.ListaTelefonica.Count() == 0)
+            {
+                Contacto conc = new Contacto(0, "João Costa", "932353371", "joaocostaworks@gmail.com", verifica);
+                listatelefonica.AdicionarContacto(conc);
+            }
             return View("Index",listatelefonica.ListaTelefonica);
         }
 
@@ -37,14 +48,32 @@ namespace ContactsManager.Controllers
         {
             verifica = true;
             Contacto conc = new Contacto(resposta.ID, resposta.Nome, resposta.Telefone, resposta.Email,verifica);
-            listatelefonica.AdicionarContacto(conc);
-            return View("adicionado",conc);
+            if (resposta.Nome == null || resposta.Telefone == null || resposta.Email == null || contemLetras(resposta.Telefone)==true)
+                return View("erro");
+            if (resposta.Nome.Length > 5 && resposta.Telefone.Length == 9 && (resposta.Email.Contains("@")))
+            {
+                listatelefonica.AdicionarContacto(conc);
+                return View("adicionado", conc);
+            }
+            else
+                return View("erro");
         }
 
         public ViewResult eliminado()
         {
            
             return View("eliminado");
+        }
+        public ViewResult erro()
+        {
+
+            return View("erro");
+        }
+
+        public ViewResult detalhes()
+        {
+
+            return View("detalhes",listatelefonica.ListaTelefonica);
         }
 
     }  
